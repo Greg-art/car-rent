079a9a5905f13154ab0acb159e9be2e1f976e5fe
Grégori Sabel <seriosabel57@gmail.com>
2022-07-18 14:00:01 -0300

    permissão por autenticação e tratativa de erros

---
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -0,0 +1,10 @@
+// add coluna avatar na table
+//  //refatorar usuario no codigo
+// config upload multer
+// criar regra de negocio do upload
+// controller
+class UpdateUserAvatarUseCase {
+  async execute() {}
+}
+
+export { UpdateUserAvatarUseCase };
