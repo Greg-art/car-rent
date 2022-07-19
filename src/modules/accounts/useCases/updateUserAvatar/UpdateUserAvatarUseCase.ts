7532fb04df3c8b822105a179549af3b87aeb0ff9
Grégori Sabel <seriosabel57@gmail.com>
2022-07-19 09:51:46 -0300

    Aula finalizada com manutenção de avatar

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -1,10 +1,30 @@
-// add coluna avatar na table
-//  //refatorar usuario no codigo
-// config upload multer
-// criar regra de negocio do upload
-// controller
+import { inject, injectable } from "tsyringe";
+
+import { deleteFile } from "../../../../utils/file";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  user_id: string;
+  avatar_file: string;
+}
+
+@injectable()
 class UpdateUserAvatarUseCase {
-  async execute() {}
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) {}
+
+  async execute({ avatar_file, user_id }: IRequest): Promise<void> {
+    const user = await this.usersRepository.findById(user_id);
+
+    if (user.avatar) {
+      await deleteFile(`./tmp/avatar/${user.avatar}`);
+    }
+    user.avatar = avatar_file;
+
+    await this.usersRepository.create(user);
+  }
 }
 
 export { UpdateUserAvatarUseCase };
